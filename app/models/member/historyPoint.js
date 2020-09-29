b9268a994134aa01e0b5194ccb1c1e310d4498a1
verrygustiandrea <verrygustiandrea@gmail.com>
2020-09-29 11:51:02 +0800

    created at history point

--- app/models/member/historyPoint.js
+++ app/models/member/historyPoint.js
@@ -5,7 +5,7 @@ module.exports = {
   getAllHistoryPoint: (id_user) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        "SELECT log_history_point.total_point_before, log_history_point.total_point_operation,log_history_point.total_point_after, log_history_point.total_spend, user.name FROM log_history_point JOIN point ON log_history_point.id_point=point.id_point JOIN user ON point.id_user=user.id_user WHERE point.id_user=? ORDER BY log_history_point.created_at DESC",
+        "SELECT log_history_point.total_point_before, log_history_point.total_point_operation,log_history_point.total_point_after, log_history_point.total_spend,  log_history_point.created_at, user.name FROM log_history_point JOIN point ON log_history_point.id_point=point.id_point JOIN user ON point.id_user=user.id_user WHERE point.id_user=? ORDER BY log_history_point.created_at DESC",
         id_user,
         (err, result) => {
           console.log(result);
