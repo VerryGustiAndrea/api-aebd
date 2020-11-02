ef66b2ca6f8cae8839cf6c0acff8c8a2dbd6d7a6
verrygustiandrea <verrygustiandrea@gmail.com>
2020-11-02 12:12:38 +0800

    receipt

--- app/models/member/historyPoint.js
+++ app/models/member/historyPoint.js
@@ -8,6 +8,11 @@ module.exports = {
         "SELECT log_history_point.email, log_history_point.order_id, log_history_point.order_picture, log_history_point.total_point_before, log_history_point.total_point_operation,log_history_point.total_point_after, log_history_point.total_spend,o.role_name AS type_order,log_history_point.description,  log_history_point.created_at FROM log_history_point JOIN (SELECT * FROM role)AS  o ON log_history_point.type_order=o.id_role  WHERE log_history_point.email=? ORDER BY log_history_point.created_at DESC",
         email,
         (err, result) => {
+          result.map((e) => {
+            if (e.order_picture != null)
+              e.order_picture =
+                `https://` + process.env.PATH_RECEIPT + e.order_picture;
+          });
           if (!err) {
             resolve(result);
           } else {
