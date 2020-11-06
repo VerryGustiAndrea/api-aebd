9e405f45a79a694595b196af9021109612624786
verrygustiandrea <verrygustiandrea@gmail.com>
2020-11-06 14:00:27 +0800

    donation

--- app/routers/v1/index.js
+++ app/routers/v1/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const member = require("./member");
 const backOffice = require("./backOffice");
-const donate = require("./donate");
+// const donate = require("./donate");
 
 // const profile = require("./profile");
 
 const Router = express.Router();
 
-Router.use("/member", member);
+Router.use("/", member);
 
 module.exports = Router;
