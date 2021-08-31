41ec5d69b300f7a0119db473c5f6d598ea231602
verrygustiandrea <verrygustiandrea@gmail.com>
2021-08-31 11:33:12 +0800

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      script: 'serverMember.js',
+      interpreter_args: ' -r ./tsconfig-paths-bootstrap.js',
+      name: 'DEV serverMember',
+
+    },
+  ],
+};
